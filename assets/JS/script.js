fb7882aacf73eec88375d14b8aa1f0f86c930585
shahan15 <k.shahan786@gmail.com>
2024-11-16 15:45:12 +0000

    buttons added, JS functions set up + background

---
+++ assets/JS/script.js
@@ -0,0 +1,37 @@
+
+//Mole hiding when clicked
+function molehidingClicked () { 
+
+}
+
+//Mole appearing randomly from ground 
+function randomMole () {
+
+}
+
+//Score counter 
+function score () {
+
+}
+
+//Play again button 
+function playAgain() {
+
+}
+
+//Easy level 
+function easy () {
+
+}
+
+//Medium level
+function medium () {
+
+}
+
+//Hard level
+function hard () {
+
+}
+
+
