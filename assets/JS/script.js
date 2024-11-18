e1d199d18d47d339974edacb75df6114061de5c8
shahan15 <k.shahan786@gmail.com>
2024-11-18 13:20:09 +0000

    mole hiding when clicked added

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,17 @@
+const moleids = ['mole1','mole2','mole3','mole4','mole5']
+
+/*goes through array of moleids and loops through them and adds 
+eventlisteners for all of them */
+moleids.forEach((id) => {
+    let mole = document.getElementById(id);
+    mole.addEventListener("click",molehidingClicked);
+})
 
 //Mole hiding when clicked
-function molehidingClicked () { 
-
+function molehidingClicked (event) { 
+    let clickedmole = event.target; // Get the clicked mole
+    clickedmole.style.transform = "translateY(50px)"; // Hide the mole
+    clickedmole.style.transition ="transform 0.2s ease";
 }
 
 //Mole appearing randomly from ground 
@@ -21,7 +31,7 @@ function playAgain() {
 
 //Easy level 
 function easy () {
-
+    
 }
 
 //Medium level
