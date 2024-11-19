93a2c6d1d4bdd80389ab8dfce03e8359190a188e
shahan15 <k.shahan786@gmail.com>
2024-11-19 14:23:59 +0000

    easy mode function set up

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,45 +1,63 @@
-const moleids = ['mole1','mole2','mole3','mole4','mole5']
-
 /*goes through array of moleids and loops through them and adds 
 eventlisteners for all of them */
+const moleids = ['mole1','mole2','mole3','mole4','mole5'];
 moleids.forEach((id) => {
     let mole = document.getElementById(id);
     mole.addEventListener("click",molehidingClicked);
 })
-
-//Mole hiding when clicked
+/**
+ * Mole moving down when clicked to mimic popping up and down
+ */
 function molehidingClicked (event) { 
     let clickedmole = event.target; // Get the clicked mole
     clickedmole.style.transform = "translateY(50px)"; // Hide the mole
-    clickedmole.style.transition ="transform 0.2s ease";
+    clickedmole.style.transition = "transform 0.2s ease";
+    clickedmole.setAttribute("data-clicked","true")
+   
 }
 
-//Mole appearing randomly from ground 
-function randomMole () {
-
-}
-
-//Score counter 
+/**
+ * score counter 
+ */
 function score () {
 
 }
 
-//Play again button 
+/**
+ * Play again button 
+ */
 function playAgain() {
 
 }
 
-//Easy level 
-function easy () {
-    
-}
+/**
+ *Easy mode 
+ */
+ function easy() {
+    const interval = setInterval(() => { //setInterval repeats a function over a specified time
+        let randomIndex = Math.floor(Math.random() * moleids.length); // Selects a random mole. Cannot use for loop as that goes in order.
+        let randomMole = moleids[randomIndex]; //indexed mole
+        let htmlMoleId = document.getElementById(randomMole); //uses indexed mole to get specific moleid from html doc
+        htmlMoleId.style.transform = "translateY(60px)"; 
+        htmlMoleId.style.transition = "transform 0.2s ease";
 
-//Medium level
+        setTimeout(() => { 
+            htmlMoleId.style.transform = "translateY(0)";
+        },600); // Delay before the mole goes back down
+    }, 2000); // Repeat every 2 seconds --> determines how often the mole pops up 
+    
+} 
+
+/**
+ * Medium level function
+ */
 function medium () {
 
 }
 
-//Hard level
+/**
+ * Hard level function
+ */
 function hard () {
 
 }
